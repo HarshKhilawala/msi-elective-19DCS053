d8fecbf32c9fa9ef2c9346046305b2de8f2ebb52
Harsh Khilawala <harsh.khilawala27@gmail.com>
2022-04-16 11:13:28 +0530

    refactor code

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const client = require('./database/db.js');
 const app = express();
+const bcrypt = require('bcrypt');
 const ports = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,17 +14,62 @@ app.use((req,res, next)=>{
   next();
 });
 
-app.get('/', (req,res)=> {
-  res.send("Root route");
-
+app.get("/", (req,res)=>{
+  let query1 = 'Select * from projects.usermanagement';
+  client.query(query1, (err,result)=>{
+    if(err){
+      res.status(400).json({"Reason":"Error in DB"});
+    }
+    else {
+      res.status(200).json(result.rows);
+    }
+    client.end;
+  });
 });
 
-app.listen(ports, function(){
-  console.log(`Server up and running on port ${ports}!`);
+app.post("/login", (req, res)=>{
+  let {username, password} = req.body;
+  let query1 = `SELECT * from projects.usermanagement where username = $1`;
+  client.query(query1,[username], (req,result)=>{
+    if(err){
+      res.status(400).json({"Reason":"Error in DB"});
+    } else if(result.rows.length===0){
+      res.send("Users does not exist.");
+    } else {
+      let userData = result.rows[0];
+      if (userData.password === password){
+        console.log("Login Successful!");
+        res.status(200).json({"Reason": "Login Successful"});
+      } else {
+        console.log("Login Failed! Username or password does not match.")
+        res.send("Login Failed!");
+      }
+    }
+
+    client.end;
+  });
+});
+
+app.post('/register', (req, res)=>{
+  let {username, password} = req.body;
+
+  // let query1 = `INSERT INTO projects.usermanagement (username, password) VALUES($1, $2)`;
+  client.query(`INSERT INTO projects.usermanagement (username, password) VALUES (${username}, ${password})`, [username, password], (err, result)=>{
+    if(err){
+      res.status(400).json({"Reason":"DB Error"});
+    } else {
+      res.status(200).json({"Reason":"User Inserted Successfully."});
+    }
+
+  });
+
+  client.end;
 });
 
 
-
+app.listen(ports,()=>{
+  console.log("Server up and running on port 3000!");
+});
 
 
 // creating a route with 2 paramaters (username, password) in request
